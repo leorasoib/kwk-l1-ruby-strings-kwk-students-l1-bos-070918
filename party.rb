8436f88e4929eec77af917530ba51a15ea17711c
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-11 17:32:25 +0000

    automatically backed up by learn

--- party.rb
+++ party.rb
@@ -1,8 +1,6 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-puts "Who are you inviting to the party?"
-guest_name= gets.chomp.capitalize
 puts "What is the name of the party?"
 party_name= gets.chomp.capitalize
 puts "When is the party happening?"
@@ -13,7 +11,13 @@ puts "Who is hosting the party?"
 host_name= gets.chomp.capitalize
 puts "When should people RSVP by?"
 rsvp=gets.chomp.capitalize
-puts "Dear #{guest_name}, 
+
+puts "Who are you inviting? Please put a space inbetween each name."
+guests= gets.chomp.capitalize
+guest_array=guests.split(" ")
+guest_array.each do |g|
+  puts "Dear #{g}, 
 You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than #{rsvp}.
 Sincerely,
 #{host_name}"
+end
